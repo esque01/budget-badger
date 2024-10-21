ce6af861eea56b1006afcbe566fcd9012b6637a0
Edward Esqueda <esquede@outlook.com>
2024-10-21 19:48:55 -0400

    Added create account page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -76,7 +76,7 @@ export default function Login() {
                     <Link href="#" variant="body2">
                         Forgot password?
                     </Link>
-                    <Link href="#" variant="body2">
+                    <Link href="#signup" variant="body2">
                         Create Account
                     </Link>
                 </div>
