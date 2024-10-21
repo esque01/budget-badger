07a07f643efd7e28dae96e71ad9038b910c809e5
Edward Esqueda <esquede@outlook.com>
2024-10-20 23:46:47 -0400

    Configured SQLite database connection

--- electron.ts
+++ electron.ts
@@ -2,9 +2,28 @@ import { app, BrowserWindow } from "electron";
 import isDev from "electron-is-dev";
 import path from "path";
 import { fileURLToPath } from "url";
+import * as dotenv from 'dotenv';
+import sqlite3 from "sqlite3"
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
+
+dotenv.config();
+
+if (!process.env.DB_NAME) {
+    throw new Error("DB_NAME is not defined in the environment variables.");
+}
+
+const dbPath = path.join(app.getPath('userData'), process.env.DB_NAME);
+
+const database = new sqlite3.Database(dbPath, (error: Error | null) => {
+    if (error) {
+        console.log('Error opening database');
+    }
+    else {
+        console.log('Connected to the SQLite database.');
+    }
+});
 
 function createWindow() {
 
@@ -22,7 +41,18 @@ function createWindow() {
         isDev ? 
         `http://localhost:3000` :
         `file://${path.join(__dirName, `../build/index.html`)}`
-    )
+    );
+
+    mainWindow.on('closed', () => {
+        database.close((error: Error | null) => {
+            if (error) {
+                console.log("Error closing SQLite database:  " + error.message);
+            }
+            else {
+                console.log('Disconnected from SQLite database.');
+            }
+        })
+    });
 }
 
 app.whenReady().then(createWindow);
