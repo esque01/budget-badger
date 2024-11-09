517bde043c1f3f54cb365b74fc72886fbd1af8cf
Edward Esqueda <esquede@outlook.com>
2024-11-08 21:16:57 -0500

    Added twilio API with client

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express, { Request, Response, Router } from "express";
 import { AccountRoutes, CheckingRoutes, ExpenseRoutes, IncomeRoutes, SavingRoutes, UserRoutes } from "./constants";
 import { login, signup, account, updateAccount } from "../controllers/UserController";
 import { createExpense, deleteExpense, getExpense, getExpenses, updateExpense } from "../controllers/ExpenseController";
@@ -7,6 +7,7 @@ import { createIncome, deleteIncome, getIncome, getIncomes, updateIncome } from 
 import { createSaving, deleteSaving, getSaving, getSavings, updateSaving } from "../controllers/SavingController";
 import { createChecking, deleteChecking, getChecking, getCheckings, updateChecking } from "../controllers/CheckingController";
 import { authRequired } from "../middleware/auth";
+import twilioClient from "../integrations/twilioClient";
 
 
 const router: Router = express.Router();
@@ -44,5 +45,18 @@ router.get(CheckingRoutes.getCheckings, authRequired, getCheckings);
 router.post(CheckingRoutes.createChecking, createChecking);
 router.put(CheckingRoutes.updateChecking, authRequired, updateChecking);
 router.delete(CheckingRoutes.deleteChecking, deleteChecking);
+
+router.post('/send-sms', (req: Request, res: Response) => {
+    const { to, body } = req.body;
+  
+    twilioClient.messages
+      .create({
+        body: body,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: to
+      })
+      .then(message => res.send(`Message sent with SID: ${message.sid}`))
+      .catch(error => res.status(500).send(error));
+  });
 
 export default router;
